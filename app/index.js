f962e9cdd5e928a3956091667cdb7b4dac02f2c1
xiaojie03 <xiaojie03@baidu.com>
2016-12-01 22:53:24 +0800

    some learn

--- app/index.js
+++ app/index.js
@@ -401,6 +401,119 @@ class SignupDialog extends React.Component {
         );
     }
 }
+/* product demo */
+class ProductCategoryRow extends React.Component {
+    render() {
+        return <tr><th colSpan="2">{this.props.category}</th></tr>;
+    }
+}
+class ProductRow extends React.Component {
+    render() {
+        var name = this.props.product.stocked ?
+            this.props.product.name :
+            <span style={{color: 'red'}}>
+                {this.props.product.name}
+            </span>;
+        return (
+            <tr>
+                <td>{name}</td>
+                <td>{this.props.product.price}</td>
+            </tr>
+        );
+    }
+}
+class ProductTable extends React.Component {
+    render() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {
+            if (product.name.indexOf(this.props.filter === -1
+                || (!product.stocked && this.props.inStockOnly)) {
+                return;
+            }
+            if (product.category !== lastCategory) {
+                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+            }
+            rows.push(<ProductRow product={product} key={product.name} />);
+            lastCategory = product.category;
+        });
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        );
+    }
+}
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange() {
+        this.props.onUserInput(
+            this.filterTextInput.value,
+            this.inStockOnlyInput.checked
+        );
+    }
+    render() {
+        return (
+            <form>
+                <input type="type" placeholder="Search..." value={this.props.filterText}
+                    ref={(input) => this.filterTextInput = input}
+                    onChange={this.handleChange} />
+                <p>
+                    <input type="checkbox"
+                        checked={this.props.inStockOnly}
+                        ref={(input) => this.inStockOnlyInput = input}
+                        onChange={this.handleChange} />
+                    {' '}
+                    Only show products in styck
+                </p>
+            </form>
+        );
+    }
+}
+class FilterableProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterText: '',
+             inStockOnly: false
+        };
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+    handleUserInput(filterText, inStockOnly) {
+        this.setState({
+            filterText: filterText,
+            inStockOnly: inStockOnly
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar filterText={this.state.filterText}
+                    inStockOnly={this.state.inStockOnly}
+                    onUserInput={this.handleUserInput} />
+                <ProductTable products={this.props.products}
+                    filterText={this.this.state.} />
+            </div>
+        );
+    }
+}
+var PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 // ReactDOM.render(<App />, document.getElementById('app'));
 // ReactDOM.render(<HelloMsg name="more"/>, document.getElementById('app'));
 // ReactDOM.render(<HelloMsg_2 />, document.getElementById('app'));
@@ -413,3 +526,4 @@ ReactDOM.render(<NameForm />, document.getElementById('root'));
 ReactDOM.render(<EasySelect />, document.getElementById('select'));
 ReactDOM.render(<PanelApp />, document.getElementById('panel'));
 ReactDOM.render(<SignupDialog />, document.getElementById('dialog'));
+ReactDOM.render(<FilterableProductTable products={PRODUCTS} />, document.getElementById('product'));
